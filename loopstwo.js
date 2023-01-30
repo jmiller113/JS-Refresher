b507583fba90fe9bce6dd82f45d814081343bf43
Miller <jazminemiller@rocketcentral.com>
2023-01-30 17:35:36 -0500

    JS Refresher

---
+++ loopstwo.js
@@ -0,0 +1,21 @@
+const firstName = "Jazmine";
+const lastName ="Miller";
+const fullName = `${firstName}  ${lastName}`;
+console.log(fullName);
+
+const population = 632000;
+console.log(population);
+
+console.log(population * 3);
+const javaScriptIsCool = true;
+const colors= ['red', 'orange', 'yellow', 'black' ];
+
+//keyword condition code
+
+for ( let i=1; i < 5; i++) {
+    console.log(i);
+}
+
+for (let i=0; i <= 4; i++) {
+    console.log(i);
+}
